45bad14c944ae310e9aafe5770ea798bd99396a3
inayjain01 <inayjain0620@gmail.com>
2025-11-13 19:47:56 -0500

    Create quiz_01.py
    Added first quiz

---
+++ quiz_01.py
@@ -0,0 +1,55 @@
+# Ques 01: What is the output of the following code snippet?
+x = 5
+x = x + 3
+print(x)
+# Answer: 8
+
+# Ques 02: What is the output of the following code snippet?
+# Whiich of these is a string?
+# A) 42
+# B) "42"
+# C) 42.0
+
+# Answer: B
+
+#Ques 03:What does this code print
+print("Hello" + " World")
+
+# Answer: Hello World!
+
+# Ques 04: What does input () do in Python?
+# Answer:   It takes user input from the console.
+
+# Ques 05: What will Python print? 
+age = 12
+if age > 10:
+    print("You are older than 10!")
+# Answer: You are older than 10!
+
+# Ques 06: How many times will this loop run?
+for i in range(4):
+    print(i)
+# Answer: 4 times (prints 0, 1, 2, 3) 
+
+# Ques 07: Is this code correct or will ti cause an error?
+# print("Hello)
+# Answer: It will cause a syntax error due to the missing closing quotation mark.
+
+# Ques 08: What is the vaule of: 
+7 * (2 + 3)
+# Answer: 35
+
+# Ques 09: Which of these is a list in Python?
+#A. ["dog", "cat", "bird"]
+#B. "dog, cat, bird"
+#. {dog, cat, bird}
+# Answer: A 
+
+# Ques 10: Write code that prints your name
+print ("My name is Inay Jain")
+
+
+# Section 2: Ques 
+
+
+
